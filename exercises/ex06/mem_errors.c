b79908c6a3f933e3152bde24afd01efd0e16e84d
Enmo Ren <enmo.ren@students.olin.edu>
2019-03-09 19:35:48 -0500

    Update mem_errors.c

--- exercises/ex06/mem_errors.c
+++ exercises/ex06/mem_errors.c
@@ -1,7 +1,29 @@
-/* Example code for Exercises in C.
+/*  Implementations of linked list in c
 
-Copyright 2014 Allen Downey
-License: Creative Commons Attribution-ShareAlike 3.0
+Author: Enmo Ren
+Copyright (c) Enmo Corporation.
+
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software.
+
+Result from running Valgrind:
+==13015== Memcheck, a memory error detector
+==13015== Copyright (C) 2002-2015, and GNU GPL'd, by Julian Seward et al.
+==13015== Using Valgrind-3.11.0 and LibVEX; rerun with -h for copyright info
+==13015== Command: ./a.out
+==13015==
+==13015==
+==13015== HEAP SUMMARY:
+==13015==     in use at exit: 0 bytes in 0 blocks
+==13015==   total heap usage: 4 allocs, 4 frees, 412 bytes allocated
+==13015==
+==13015== All heap blocks were freed -- no leaks are possible
+==13015==
+==13015== For counts of detected and suppressed errors, rerun with: -v
+==13015== ERROR SUMMARY: 0 errors from 0 contexts (suppressed: 0 from 0)
 
 */
 
@@ -21,15 +43,17 @@ int read_element(int *array, int index) {
 
 int main()
 {
-    int never_allocated;
+    // int never_allocated; This variable is never used, I commented it out
     int *free_twice = malloc(sizeof (int));
     int *use_after_free = malloc(sizeof (int));
     int *never_free = malloc(sizeof (int));
+
     int array1[100];
     int *array2 = malloc(100 * sizeof (int));
 
     // valgrind does not bounds-check static arrays
-    read_element(array1, -1);
+    // read_element(array1, -1); Commented it out to make sure read element in bound
+    read_element(array1, 1);
     read_element(array1, 100);
     // free(array1);
 
