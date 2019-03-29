a18ed9f074b2eb926ae136999af3c1ce4f0150de
Enmo Ren <enmo.ren@students.olin.edu>
2019-03-29 16:38:59 -0400

    Finished exercise 08

--- exercises/ex08/matrix.c
+++ exercises/ex08/matrix.c
@@ -30,7 +30,11 @@ Matrix *make_matrix(int num_rows, int num_cols) {
 /* Free a matrix.
 */
 void free_matrix(Matrix *matrix) {
-    // TODO: Fill this in.
+    for (int i=0; i<matrix->num_rows; i++){
+      free(matrix->rows[i]);
+    }
+    free(matrix->rows);
+    free(matrix);
 }
 
 /* Print a row of a matrix.
@@ -56,7 +60,12 @@ Subtract a multiple of row j from row i so that the first element
 of row i is 0.
 */
 void reduce_matrix_rows(Matrix *matrix, int i, int j) {
-    // TODO: Fill this in.
+    double *i_ptr = matrix->rows[i];
+    double *j_ptr = matrix->rows[j];
+    double multiple = i_ptr[0]/j_ptr[0];
+    for (int c = 0; c< matrix->num_cols; c++){
+      i_ptr[c] = i_ptr[c] - j_ptr[c]*multiple;
+    }
 }
 
 int main () {
